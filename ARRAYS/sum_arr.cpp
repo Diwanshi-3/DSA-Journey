1e4ace79f3f4236ca07d28a67a745c81dd776f2c
Diwanshi-3 <diwanshichhabra5@gmail.com>
2026-03-21 22:23:06 +0530

    Sum of elements of array

--- ARRAYS/sum_arr.cpp
+++ ARRAYS/sum_arr.cpp
@@ -9,13 +9,13 @@ int sum(int arr[],int n){
 }
 int main(){
     int n;
-    cout<<"Enter size";
+    cout<<"Enter size:";
     cin>>n;
     int arr[n];
     cout<<"Enter elements:";
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    sum(arr,n);
+    cout<<sum(arr,n);
     return 0;
 }
