82ec0193d1d24af174cbfd62439b4f180257fc78
Diwanshi-3 <diwanshichhabra5@gmail.com>
2026-05-24 11:28:41 +0530

    delete an element from end

---
+++ ARRAYS/deletion_end.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[100]={10,20,30,40};
+    int size=4;
+    size=size-1;
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
